c18256744dfcdbc6f010201d4f75069cad31fb77
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-27 17:27:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+// function isSeven(x) {
+// 	return x="7"?false:true:false;
+// }
+
+function isSeven(x) {
+	return x === 7 ? true : false ;
+}
+console.log(isSeven(4));
+console.log(isSeven(9));
+console.log(isSeven(7));
